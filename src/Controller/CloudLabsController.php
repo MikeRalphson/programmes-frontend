b46bd0d4a3958a247a6ac19382e4b2f3e8cc03df
Bjorn Twachtmann <bjorn.twachtmann@bbc.co.uk>
2017-04-25 16:09:01 +0100

    Proof-of-concept dynamic domain model class instantiation

--- src/Controller/CloudLabsController.php
+++ src/Controller/CloudLabsController.php
@@ -8,6 +8,10 @@ class CloudLabsController extends BaseController
 {
     public function __invoke(Request $request)
     {
+        $atozService = $this->get('BBC\\ProgrammesPagesService\\Service\\AtozTitlesService');
+        $tleos = $atozService->findTleosByFirstLetter('A', 20, 1);
+        var_dump($tleos);
+        die();
         return $this->render('cloud_labs/show.html.twig');
     }
 }
---
+++ src/Domain/AtozTitle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Domain;
+
+use \BBC\ProgrammesPagesService\Domain\Entity\AtozTitle as AtozTitleDomain;
+
+class AtozTitle extends AtozTitleDomain
+{
+
+}
