ff7ea04e1abb510467bde0c516f30fa18a97b55a
Francisco Albusac <francisco.albusac@bbc.co.uk>
2017-04-28 12:33:46 +0100

    New more simple approach

--- src/Controller/SchedulesByDayController.php
+++ src/Controller/SchedulesByDayController.php
@@ -14,30 +14,12 @@ class SchedulesByDayController extends BaseController
     public function __invoke(Pid $pid, ?string $date)
     {
         if (!is_null($date)) {
-            $validator = $this->get('validator');
-
-            // TODO see if we can create a valid date from $date else throw 404
-            $errors = $validator->validate(
-                $date,
-                [new Date()]
-            );
-
-            if (count($errors) > 0) throw new HttpException(404, "No found results for this date: invalid date");
-
             $date = DateTime::createFromFormat('Y-m-d', $date);
-
-            // TODO make sure date is in a sensible range - year is between 1900
-            // and 2100 else throw 404 (no point querying the DB for dates we know won't have any results)
-            $errors = $validator->validate(
-                $date->format('Y'),
-                [new Range(['min' => 1900, 'max'=>2100])]
-            );
-
-            if (count($errors) > 0) throw new HttpException(404, "No found results for this date: date out of range");
-
-
+            $dateErrors = DateTime::getLastErrors();
+            if ($dateErrors['warning_count'] != 0 || $dateErrors['error_count'] != 0 || $date->format('Y') < 1900) {
+                throw new HttpException(404, 'Invalid date');
+            }
         } else {
-            // TODO if date is absent use today's date
             $date = new DateTime();
         }
 
